4c440918d60008d6f373640d27e50a1c888b8b28
3bdlmwgood <abdullahmohamedabdelmawgood@gmail.com>
2026-09-04 18:54:19 +0300

    Solve Q1

--- Assignment_2/Assignment_2/Program.cs
+++ Assignment_2/Assignment_2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Q1
+
+        int number;
+        bool isValidInput = false;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            isValidInput = int.TryParse(Console.ReadLine(), out number);
+        }
+        while (!isValidInput);
+        
+        if (number % 3 == 0 && number % 4 == 0)
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("No");
+
+        #endregion
+
+
+
     }
 }
