364bf2c5e6e7d3eeef31abfc1602b3825f0d6cd8
3bdlmwgood <abdullahmohamedabdelmawgood@gmail.com>
2026-09-04 15:52:55 +0300

    Solve Question 1

--- Assignment_2/ConsoleApp1/Program.cs
+++ Assignment_2/ConsoleApp1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Q1
+
+        int number;
+        bool flag;
+        do
+        {
+            Console.Write("Enter The Number :");
+            flag = int.TryParse(Console.ReadLine(), out number);
+
+        } while (!flag);
+
+        Console.WriteLine($"Entered Number is [ {number} ]");
+
+        #endregion
     }
 }
