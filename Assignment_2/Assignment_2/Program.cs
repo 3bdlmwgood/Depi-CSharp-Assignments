5bddec0f19943704ea9bb7b3a1c4b29782caa5ab
3bdlmwgood <abdullahmohamedabdelmawgood@gmail.com>
2026-09-04 19:44:48 +0300

    Solve Q13

--- Assignment_2/Assignment_2/Program.cs
+++ Assignment_2/Assignment_2/Program.cs
@@ -217,6 +217,18 @@ class Program
         #endregion
 
 
+        #region Q13
+
+        Console.Write("Enter a string: ");
+        string text = Console.ReadLine();
+
+
+        for (int i = text.Length - 1; i >= 0; i--)
+        {
+            Console.Write(text[i]);
+        }
+
+        #endregion
 
 
     }
